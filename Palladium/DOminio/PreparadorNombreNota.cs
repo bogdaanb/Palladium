e94a6d9412a91da5bf76af4b9cced96f79223873
Raúl Luján <domenec.raul@gmail.com>
2026-08-28 14:50:02 +0200

    crear archivo.md sin contenido de momento

---
+++ Palladium/DOminio/PreparadorNombreNota.cs
@@ -0,0 +1,24 @@
+namespace Palladium.Dominio.Archivos;
+
+public static class PreparadorNombreNota
+{
+    public static string Preparar(string? nombreNota)
+    {
+        if (string.IsNullOrWhiteSpace(nombreNota))
+        {
+            throw new ArgumentException(
+                "El nombre no puede estar vacío.");
+        }
+
+        string nombreLimpio = nombreNota.Trim();
+
+        if (nombreLimpio.EndsWith(
+                ".md",
+                StringComparison.OrdinalIgnoreCase))
+        {
+            return nombreLimpio;
+        }
+
+        return nombreLimpio + ".md";
+    }
+}
--- Palladium/Program.cs
+++ Palladium/Program.cs
@@ -1,3 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Palladium.Dominio.Archivos;
 
-Console.WriteLine("Hello, World!");
+Console.Write("Introduce el nombre de la nota: ");
+string? nombreNota = Console.ReadLine();
+
+string nombreArchivo =
+    PreparadorNombreNota.Preparar(nombreNota);
+
+string carpetaNotas = "Notas";
+
+Directory.CreateDirectory(carpetaNotas);
+
+string rutaCompleta =
+    Path.Combine(carpetaNotas, nombreArchivo);
+
+File.WriteAllText(rutaCompleta, "");
+
+Console.WriteLine("Nota creada correctamente.");
+Console.WriteLine("Ruta: " + rutaCompleta);
